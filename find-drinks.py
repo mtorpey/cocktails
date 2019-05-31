7bf8f01f9cbef940b85a9b6e503c7612ea1d05b8
Michael Torpey <mct25@st-andrews.ac.uk>
2019-05-31 16:36:25 +0100

    Multiple purchase suggestions

--- find-drinks.py
+++ find-drinks.py
@@ -49,15 +49,59 @@ for type in sorted(types):
         if drink['type'] == type:
             print('  ' + drink['name'] + ' - ' + ', '.join(drink['ingredients']))
 
-# Find enabling ingredients
-to_buy = {}
-for recipe in recipes:
-    if len(recipe['missing']) == 1:
-        to_buy[next(iter(recipe['missing']))] = []
-for recipe in recipes:
-    if len(recipe['missing']) == 1:
-        to_buy[next(iter(recipe['missing']))].append(recipe['name'])
-print('\nGOOD INGREDIENTS TO BUY:')
-for tb in sorted(to_buy, key=(lambda i: [-len(to_buy[i]), i])):
-    print('  (' + str(len(to_buy[tb])) + ') ' + tb + ' - ' + ', '.join(to_buy[tb]))
+# Helper function for computing covers
+def all_covers(sets, size):
+    """Return the sets of size no more than `size` that cover as many of `sets` as possible"""
+    unique_sets = []
+    [unique_sets.append(s) for s in sets if len(s) <= size and s not in unique_sets]
+    unions = unique_sets.copy()
+    covers = []
+    i = 0
+    while i < len(unions):
+        covers.append(0)
+        for s in sets:
+            if s.issubset(unions[i]):
+                covers[i] += 1
+        for s in unique_sets:
+            u = unions[i].union(s)
+            if len(u) <= size and u not in unions:
+                unions.append(u)
+        i += 1
+    if unions == []:
+        return []
+    return sorted(unions, key=lambda s: (-covers[unions.index(s)], len(s), list(s)))
 
+def best_covers(sets, size):
+    unions = all_covers(sets, size)
+    best_unions = []
+    lastcover = None
+    for u in unions:
+        cover = 0
+        for s in sets:
+            if s.issubset(u):
+                cover += 1
+        if lastcover == None or cover == lastcover:
+            best_unions.append(u)
+            lastcover = cover
+        else:
+            break
+    return best_unions
+
+incomplete_recipes = [recipe for recipe in recipes if len(recipe['missing']) > 0]
+missing_sets = [recipe['missing'] for recipe in incomplete_recipes]
+
+# Find enabling ingredients
+print("\nIF YOU'RE BUYING ONE INGREDIENT:")
+best = all_covers(missing_sets, 1)
+for s in best:
+    allows = [recipe['name'] for recipe in incomplete_recipes
+              if all([ing in s for ing in recipe['missing']])]
+    print('  (' + str(len(allows)) + ') ' + ', '.join(sorted(s)) + ' - ' + ', '.join(allows))
+
+# Find ingredient sets
+print("\nIF YOU'RE BUYING TWO INGREDIENTS:")
+best = best_covers(missing_sets, 2)
+for s in best:
+    allows = [recipe['name'] for recipe in incomplete_recipes
+              if all([ing in s for ing in recipe['missing']])]
+    print('  (' + str(len(allows)) + ') ' + ', '.join(sorted(s)) + ' - ' + ', '.join(allows))
